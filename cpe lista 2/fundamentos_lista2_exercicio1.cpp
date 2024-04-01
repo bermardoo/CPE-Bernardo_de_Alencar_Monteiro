b224ddd3e64e466667700e6e3ebf02fcd6ed4898
bermardoo <133049455+bermardoo@users.noreply.github.com>
2024-03-31 23:13:19 -0300

    Add files via upload

---
+++ cpe lista 2/fundamentos_lista2_exercicio1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	//definir variaveis
+	int x= 7 + 3 * 6/2 - 1;
+	int y= 2 % 2 + 2 * 2/2;
+	int z= (3*9*(3+(9*3/(3))));
+	
+	//imprimir variaveis
+	
+	cout<< x << endl << y << endl << z;
+	
+	return 0;
+}
---
+++ cpe lista 2/fundamentos_lista2_exercicio3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int r;
+	
+	cout<< "por favor, digite um raio de circulo: " << endl;
+	cin >> r;
+	
+	cout << "aqui esta o diametro da circunferencia: " << 2*3.14159*r << endl << "e aqui esta a area: " << 3.14159*r*r << endl;
+	
+	return 0;
+}
